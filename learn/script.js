9d8b7a0149143c18b6c0c03cf0a6ac4a0b4f01c8
LaoWang1518 <18562820167@163.com>
2024-11-02 13:24:57 +0800

    1.5.0

---
+++ learn/script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("app").setAttribute('style','background-color:#000000;');
+document.getElementById('page-index').setAttribute('style','color:#ffffff;');
+document.getElementsByClassName('s-space')[0].setAttribute('style','background-color:#000000;');
+document.getElementsByClassName('col-1')[0].setAttribute('style','background-color:#000000;');
+document.getElementsByClassName('col-2')[0].setAttribute('style','background-color:#000000;');
+document.getElementsByClassName('section elec')[0].setAttribute('style','background-color:#000000;');
+document.getElementsByClassName('section i-live')[0].setAttribute('style','background-color:#000000;');
+document.getElementsByClassName('section user-info')[0].setAttribute('style','background-color:#000000;');
+document.getElementsByClassName('wrapper')[0].setAttribute('style','background-color:#000000;');
+document.getElementsByClassName('n-inner clearfix')[0].setAttribute('style','background-color:#000000;');
